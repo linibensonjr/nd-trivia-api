4568b1f1e1418f744c2f5642697834eeb6426ec5
Iniobong Benson <linibensonjr@gmail.com>
2022-06-26 00:16:58 +0100

    Clean up code

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,4 +1,5 @@
 import os
+from sys import dllhandle
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
@@ -14,8 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_name = "trivia"
+        self.database_path = "postgresql://{}:{}@{}/{}".format('ustudent','iniobong','localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +34,57 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["categories"])
+
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["questions"])
+        self.assertTrue(data["categories"])
+
+    def test_404_page_not_found(self):
+        res = self.client().get("/questions?page=500")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], 'Not Found')
+
+    def test_delete_question(self):
+        res = self.client().delete('/questions/42')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(data["deleted"], 42)
+
+
+    def test_add_question(self):
+        add_question = {
+            'question': 'What is the most popular cyptocurrency?',
+            'answer': 'Bitcoin',
+            'difficulty': 1,
+            'category': 1
+        }
+        res = self.client().post('/questions', json=add_question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+
+
+    def test_search(self):
+        search = {'searchTerm': 'Which', }
+        res = self.client().post('/search', json=search)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['questions']), 7)
 
 
 # Make the tests conveniently executable
