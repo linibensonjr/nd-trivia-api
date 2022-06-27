87c306b59288e2896db96c7b5e2a8affe9e2e778
Iniobong Benson <linibensonjr@gmail.com>
2022-06-27 17:47:35 +0100

    Complete all tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -19,6 +19,13 @@ class TriviaTestCase(unittest.TestCase):
         self.database_path = "postgresql://{}:{}@{}/{}".format('ustudent','iniobong','localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
+        self.test_question = {
+                    "question": "When was Jerusalem destroyed by the Babylonians",
+                    "answer": "587 BCE",
+                    "category": "4",
+                    "difficulty": "4"
+                }
+
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -37,17 +44,21 @@ class TriviaTestCase(unittest.TestCase):
     def test_get_categories(self):
         res = self.client().get('/categories')
         data = json.loads(res.data)
+
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
         self.assertTrue(data["categories"])
 
+
     def test_get_paginated_questions(self):
         res = self.client().get('/questions')
         data = json.loads(res.data)
+
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
         self.assertTrue(data["questions"])
         self.assertTrue(data["categories"])
+
 
     def test_404_page_not_found(self):
         res = self.client().get('/questions?page=500')
@@ -57,48 +68,95 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data["success"], False)
         self.assertEqual(data["message"], 'Not Found')
 
+
     def test_delete_question(self):
-        res = self.client().delete('/questions/42')
+        res = self.client().delete('/questions/16')
         data = json.loads(res.data)
+
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
-        self.assertEqual(data["deleted"], 42)
+        self.assertEqual(data["deleted"], '16')
+
+
+    def test_422_if_question_does_not_exist(self):
+        res = self.client().delete('/questions/1500')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "Unprocessable")
 
 
     def test_add_question(self):
         add_question = {
             'question': 'What is the most popular cyptocurrency?',
             'answer': 'Bitcoin',
-            'difficulty': 1,
-            'category': 1
+            'difficulty': '1',
+            'category': '4'
         }
         res = self.client().post('/questions', json=add_question)
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
+        self.assertTrue(data["questions"])
+
+    def test_405_unable_to_add_question(self):
+        res = self.client().post('/questions/34', json=self.test_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "Method Not Allowed")
 
 
     def test_search(self):
-        search = {'searchTerm': 'Which', }
+        search = {'searchTerm': 'Which'}
         res = self.client().post('/search', json=search)
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(len(data['questions']), 7)
+        self.assertTrue(data["questions"])
+        self.assertEqual(len(data['questions']), 5)
+
+
+    def test_search_not_found(self):
+        search = {'searchTerm': 'someterm'}
+        res = self.client().post('/search', json=search)
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data["questions"], 0)
+        self.assertEqual(len(data['questions']), 0)
+
 
     def test_start_quiz(self):
-        quiz_question = {
-            'previous_question': [],
-            'quiz_category': {
-                'type': 'Sport',
-                'id': '6'
-            }
+        quiz_question = {'previous_questions': [9],'quiz_category': {
+                'type': 'Geography',
+                'id': '4'}
         }
         res = self.client().post('/quizzes', json=quiz_question)
         data = json.loads(res.data)
+        
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+
+        self.assertEqual(data['question']['category'], 4)
+        self.assertEqual(data['question']['id'], 65)
         
+
+    def test_404_start_quiz(self):
+        quiz_question = {'previous_questions': [7],
+            'quiz_category': {
+                'type': 'Historia',
+                'id': '9'}
+        }
+        res = self.client().post('/quizzes', json=quiz_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
 
 
 
